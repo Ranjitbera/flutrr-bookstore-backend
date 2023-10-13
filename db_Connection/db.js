46aa5637bf4eb4fcb7188f57ae728e7b30b3ba09
Ranjitbera <124592068+Ranjitbera@users.noreply.github.com>
2023-10-14 02:46:08 +0530

    Update db.js

--- db_Connection/db.js
+++ db_Connection/db.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 
 const db_url = process.env.DATABASE;
@@ -15,22 +15,5 @@ module.exports = () => {
 		console.log(error);
 		console.log("Could not connect database!");
 	}
-=======
-const mongoose = require('mongoose');
 
-const db_url = process.env.DATABASE;
-
-module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect(db_url, connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
->>>>>>> origin/main
 };
